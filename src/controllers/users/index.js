7f5a92b05d37165a66ce2f13618b3701416144e5
Aditya Tyagi <aditya.tyagi@ebabu.co>
2020-01-13 19:06:42 +0530

    CRUD completed for users

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,5 +1,6 @@
 const db = require('../../db');
 
+// get all users
 const getAllUsers = async (req, res, next) => {
     try {
         const query = 'SELECT * FROM users';
@@ -12,6 +13,78 @@ const getAllUsers = async (req, res, next) => {
     }
 }
 
+// create new user
+const createNewUser = async (req, res, next) => {
+    try {
+        const query = 'INSERT INTO users(name, password) VALUES($1, $2) RETURNING user_id, name';
+        const {
+            name,
+            password
+        } = req.body;
+        const {
+            rows
+        } = await db.query(query, [name, password]);
+        return res.send(rows[0]);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+// update user - username and password
+const updateUser = async (req, res, next) => {
+    try {
+        const query = 'UPDATE users SET name = $1, password = $2 WHERE user_id = $3';
+        const {
+            user_id,
+            name,
+            password
+        } = req.body;
+        const rows = await db.query(query, [name, password, user_id]);
+        return res.send(rows[0]);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+// get a particular user by user id
+const getUserByUserId = async (req, res, next) => {
+    try {
+        const query = 'SELECT * FROM users WHERE user_id = $1';
+        const {
+            user_id
+        } = req.params;
+        const {
+            rows
+        } = await db.query(query, [user_id]);
+        return res.send(rows[0]);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+// delete user by user id
+const deleteUserByUserId = async (req, res, next) => {
+    try {
+        const query = 'DELETE FROM users WHERE user_id = $1';
+        const {
+            user_id
+        } = req.params;
+        const {
+            rows
+        } = await db.query(query, [user_id]);
+        return res.send({
+            status: 200,
+            message: 'User deleted successfully!'
+        });
+    } catch (error) {
+        return next(error);
+    }
+}
+
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    createNewUser,
+    updateUser,
+    getUserByUserId,
+    deleteUserByUserId
 }
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -3,6 +3,18 @@ const router = new Router();
 
 const users = require('../../controllers').userController;
 
-router.get('/getAllUsers', users.getAllUsers);
+const getAllUsers = router.get('/getAllUsers', users.getAllUsers);
+const createNewUser = router.post('/createNewUser', users.createNewUser);
+const updateUser = router.put('/updateUser', users.updateUser);
+const getUserByUserId = router.get('/getUserByUserId/:user_id', users.getUserByUserId);
+const deleteUserByUserId = router.delete('/deleteUserByUserId/:user_id', users.deleteUserByUserId);
 
-module.exports = router;
+
+
+module.exports = [
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  getUserByUserId,
+  deleteUserByUserId
+];
