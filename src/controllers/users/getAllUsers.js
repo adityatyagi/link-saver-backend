302f1948a7d46ccb24ca5da0f78423629116ac8e
Aditya Tyagi <tyagi.aditya844747@gmail.com>
2020-01-29 12:26:29 +0530

    pagination done e with variable limit

--- src/controllers/users/getAllUsers.js
+++ src/controllers/users/getAllUsers.js
@@ -4,12 +4,32 @@ const utility = require('../../global_functions');
 // get all users
 const getAllUsers = async (req, res, next) => {
   try {
-    const query = 'SELECT user_id, name, email FROM users';
+    const {
+      page,
+      limit
+    } = req.query;
+
+    let currentPage = page ? page : 1;
+    let itemsPerPage = limit ? limit : 10;
+
+    currentPage = (currentPage - 1) * itemsPerPage;
+
+    let totalCount = await db.rowCountInTable('users');
+    let lastPage = Math.ceil(totalCount / itemsPerPage);
+
+    const query = 'SELECT user_id, name, email FROM users limit $1 offset $2';
+
     const {
       rows
-    } = await db.query(query);
+    } = await db.query(query, [itemsPerPage, currentPage]);
 
-    return utility.successResponse(res, rows, 'All users fetched successfully');
+    const resData = {
+      usersList: rows,
+      lastPage: lastPage,
+      totalCount: +totalCount
+    }
+
+    return utility.successResponse(res, resData, 'All users fetched successfully');
   } catch (error) {
     return utility.badRequestError(error, "Failed to get users list");
   }
